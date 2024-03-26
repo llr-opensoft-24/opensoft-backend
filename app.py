8df0a1f2a05bef93e9c58a372e348b6bd8d86b8e
Nithin Siriguppa <85257314+nithin-gith@users.noreply.github.com>
2024-03-26 22:48:06 +0530

    Revert "Auth Routes updated"

--- app.py
+++ app.py
@@ -4,23 +4,19 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 from flask_bcrypt import Bcrypt
-from models import User, Verification
-from flask_mail import Mail, Message
-import random, string
+from models import User
 import datetime
 import arrow
 import jwt
-
-
 import razorpay
 import hmac
 from functools import wraps
 import hashlib
 
-
 load_dotenv()
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+
 DB_URI = os.getenv('DB_URI')
 RAZORPAY_ID = os.getenv('RAZORPAY_ID')
 RAZORPAY_KEY_SECRET = os.getenv('RAZORPAY_KEY_SECRET')
@@ -31,24 +27,11 @@ AUTO_COMPLETE_INDEX_NAME = os.getenv("AUTO_COMPLETE_INDEX_NAME")
 
 try:
     client = MongoClient(DB_URI)
-    print("Connected to MongoDB successfully!")
     db = client['sample_mflix']
-    
-    if "user" not in db.list_collection_names():
-        db.create_collection("user")
+    print("Connected to MongoDB successfully!")
 
-    if "verification" not in db.list_collection_names():
-        db.create_collection("verification")
-
-    user_collection = db["user"]
-    verification_collection = db["verification"]
-    app.config["MAIL_SERVER"] = "smtp.gmail.com"
-    app.config["MAIL_PORT"] = 465
-    app.config["MAIL_USE_TLS"] = False
-    app.config["MAIL_USE_SSL"] = True
-    app.config["MAIL_USERNAME"] = os.getenv("GMAIL_USERNAME")
-    app.config["MAIL_PASSWORD"] = os.getenv("GMAIL_APP_PASSWORD")
-    mail = Mail(app)
+    # if "user" not in db.list_collection_names():
+    #     db.create_collection("user")
     
 
 except ConnectionFailure as e:
@@ -62,22 +45,6 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
-
-
-def generate_otp():
-    otp = "".join(random.choices(string.digits, k=6))
-    print(otp)
-    return otp
-
-
-def send_otp_email(email, otp):
-    msg = Message(
-        "OTP for registration",
-        sender=os.getenv("GMAIL_USERNAME"),
-        recipients=[email],
-    )
-    msg.body = f"Your OTP for registration is {otp}"
-    mail.send(msg)
 
 
 def verify_token(func):
@@ -110,7 +77,6 @@ def verify_token(func):
         return func(user_id, *args, **kwargs)
 
     return decorated_function
-
 
 
 
@@ -203,6 +169,7 @@ def register():
             "utf-8"
         )
         data["password"] = hashed_password
+        user_collection = db["user"]
         if user_collection.find_one({"email": data["email"]}):
             response["error"]="User already exists!"
             response["message"] = "User already exists!"
@@ -225,38 +192,10 @@ def login():
     if request.method == "POST":
         data = request.get_json()
         user_collection = db["user"]
-        print(data)
         user_data = user_collection.find_one({"email": data["email"]})
-        print(user_data)
+
         if user_data:
             if bcrypt.check_password_hash(user_data["password"], data["password"]):
-
-                if user_data["verified"]:
-                    payload = {
-                        "user_id": str(user_data["_id"]),
-                        "exp": arrow.utcnow().shift(minutes=5).int_timestamp,
-                    }
-                    print("Payload: ",payload)
-                    token = jwt.encode(payload, os.getenv("SECRET_KEY"), algorithm="HS256")
-                    token = str(token)
-                    del user_data["password"]
-                    user_data["_id"] = str(user_data["_id"])
-
-                    response["results"] = {"user_data": user_data, "token": token}
-                    return response
-                else:
-                    otp = generate_otp()
-                    print("OTP: ",otp)
-                    send_otp_email(data["email"], otp)
-                    print("OTP sent successfully! Please verify OTP to complete registration.")
-                    ver = Verification(email=data["email"],otp=otp,otptype="email",generationtime=datetime.datetime.utcnow())
-                    verification_collection.insert_one(ver.to_mongo())
-                    user1 = user_data
-                    user1["_id"] = str(user1["_id"])
-                    del user1["password"]
-                    response["results"]={"message":"OTP sent successfully! Please verify OTP to complete registration.","token":None,"user_data":user1}
-                    return response        
-
                 payload = {
                     "user_id": str(user_data["_id"]),
                     "exp": arrow.utcnow().shift(minutes=1440).int_timestamp,
@@ -268,76 +207,12 @@ def login():
 
                 response["data"] = {"user_data": user_data, "token": token}
                 return response
-
             else:
                 response["error"] = "Invalid credentials!"
                 response["message"] = "Email / Password is incorrect"
                 return response, 401
         else:
             response["error"] = "User not found!"
-            return response
-    response["error"] = "Method not allowed!"
-    return response
-
-
-@app.route("/verifyemail",methods=["POST"])
-def verifyemail():
-    response = {"error": None, "results": {}}
-
-    if request.method == "POST":
-        data = request.get_json()
-        user_collection = db["user"]
-        print(data)
-        user_data = user_collection.find_one({"email": data["email"]})
-        veridata = verification_collection.find_one({"email": data["email"]})
-        print(veridata)
-        otp_entered = veridata["otp"]
-
-        if user_data:
-            if "otp" in data and str(data["otp"]) == str(otp_entered) and veridata["otptype"]=="email" and veridata["generationtime"]>datetime.datetime.utcnow()-datetime.timedelta(minutes=5):
-                del veridata["otp"]
-                payload = {
-                    "user_id": str(user_data["_id"]),
-                    "exp": arrow.utcnow().shift(minutes=5).int_timestamp,
-                }
-                token = jwt.encode(payload, os.getenv("SECRET_KEY"), algorithm="HS256")
-                token = str(token)
-                user_collection.update_one({"email":data["email"]},{"$set":{"verified":True}})
-                user_data["verified"] = True
-                del user_data["password"]
-                user_data["_id"] = str(user_data["_id"])
-                response["results"] = {"user_data": user_data, "token": token}  
-                return response
-            else:
-                print(otp_entered)
-                # print(user_data["otp"])
-                return "Invalid OTP!", 401
-        else:
-            return "User not found!", 404
-        
-        
-@app.route("/forgetpassword",methods=["POST"])
-def forgetpassword():
-    response = {"error": None, "results": {}}
-
-    if request.method == "POST":
-        data = request.get_json()
-        user_collection = db["user"]
-        user_data = user_collection.find_one({"email": data["email"]})
-
-        if user_data:
-            otp = generate_otp()
-            send_otp_email(data["email"], otp)
-            data["otp"] = otp
-            ver = Verification(email=data["email"],otp=otp,otptype="password",generationtime=datetime.datetime.utcnow())
-            verification_collection.insert_one(ver.to_mongo())
-            response["results"] = {"message": "OTP sent successfully! Please verify OTP to reset password."}
-            return response
-        else:
-            response["error"] = "User not found!"
-            return response
-        
-    response["error"] = "Method not allowed!"
             response["message"] = "No user exists with given email"
             return response, 404
     
--- models.py
+++ models.py
@@ -9,18 +9,11 @@ import datetime
 
 
 class User(Document):
+    username = StringField(required=True, unique=True)
     email = EmailField(required=True, unique=True)
-    username = StringField()
-    password = StringField(default="abbadabbajabba")
+    password = StringField(required=True)
     createdat = DateTimeField(default=datetime.datetime.max)
     updatedat = DateTimeField(default=datetime.datetime.max)
     plan = StringField(default="free")
     verified = BooleanField(default=False)
-
-
-class Verification(Document):
-    email = EmailField(required=True, unique=True)
-    otp = StringField(required=False)
-    otptype = StringField(default="")
-    generationtime = DateTimeField(default=datetime.datetime.utcnow)
 
